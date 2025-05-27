633b3a8c602106757e06cd041545add8198264e7
JohnCelery <unwound_messes2v@icloud.com>
2025-05-27 14:01:25 -0400

    Update assets.js

--- src/assets.js
+++ src/assets.js
@@ -1,19 +1,19 @@
-export const parchmentTile = 'PASTE_URL_HERE';
+export const parchmentTile = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2010_02_22%20AM.png?v=1748367046972';
 export const leatherTile = 'PASTE_URL_HERE';
 export const mapVignette = 'PASTE_URL_HERE';
 
-export const europeMap = 'PASTE_URL_HERE';
-export const nodeDefault = 'PASTE_URL_HERE';
-export const nodeCurrent = 'PASTE_URL_HERE';
-export const nodeVisited = 'PASTE_URL_HERE';
-export const marker = 'PASTE_URL_HERE';
+export const europeMap = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2001_55_50%20PM.png?v=1748368562677';
+export const nodeDefault = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2009_19_11%20AM.png?v=1748367014349';
+export const nodeCurrent = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2009_21_24%20AM.png?v=1748367018581';
+export const nodeVisited = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2009_20_40%20AM.png?v=1748367016509';
+export const marker = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2009_25_09%20AM.png?v=1748366995667';
 export const markerShadow = 'PASTE_URL_HERE';
 
-export const iconMarket = 'PASTE_URL_HERE';
-export const iconMonastery = 'PASTE_URL_HERE';
-export const iconFarmland = 'PASTE_URL_HERE';
-export const iconForest = 'PASTE_URL_HERE';
-export const iconMountain = 'PASTE_URL_HERE';
+export const iconMarket = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2011_02_45%20AM.png?v=1748367052894';
+export const iconMonastery = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2001_28_26%20PM.png?v=1748367059119';
+export const iconFarmland = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2001_28_30%20PM.png?v=1748367062882';
+export const iconForest = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2001_45_07%20PM.png?v=1748368276915';
+export const iconMountain = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2001_49_41%20PM.png?v=1748368286962';
 export const iconRiver = 'PASTE_URL_HERE';
 export const iconPort = 'PASTE_URL_HERE';
 export const iconRuin = 'PASTE_URL_HERE';
@@ -24,19 +24,19 @@ export const iconCathedral = 'PASTE_URL_HERE';
 
 export const btnPlank = 'PASTE_URL_HERE';
 export const tooltipPanel = 'PASTE_URL_HERE';
-export const modalFrame = 'PASTE_URL_HERE';
+export const modalFrame = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2009_47_38%20AM.png?v=1748367037214';
 export const diaryPanel = 'PASTE_URL_HERE';
 export const scorePanel = 'PASTE_URL_HERE';
 export const deathPanel = 'PASTE_URL_HERE';
 
-export const hudFoodRing = 'PASTE_URL_HERE';
-export const hudWaterRing = 'PASTE_URL_HERE';
+export const hudFoodRing = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2009_41_01%20AM.png?v=1748367031978';
+export const hudWaterRing = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2009_44_21%20AM.png?v=1748367033686';
 export const meterShadow = 'PASTE_URL_HERE';
 
 export const gridCell = 'PASTE_URL_HERE';
-export const grainSack = 'PASTE_URL_HERE';
-export const waterskin = 'PASTE_URL_HERE';
-export const toolkit = 'PASTE_URL_HERE';
+export const grainSack = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2009_29_24%20AM.png?v=1748367026388';
+export const waterskin = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2009_31_06%20AM.png?v=1748367028535';
+export const toolkit = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2009_36_49%20AM.png?v=1748367030270';
 
 export const grainFieldTile = 'PASTE_URL_HERE';
 export const forestWoodTile = 'PASTE_URL_HERE';
@@ -45,11 +45,11 @@ export const riverFishTile = 'PASTE_URL_HERE';
 export const scrapHeapTile = 'PASTE_URL_HERE';
 export const mosaicShardTile = 'PASTE_URL_HERE';
 
-export const heroMarket = 'PASTE_URL_HERE';
-export const heroMonastery = 'PASTE_URL_HERE';
-export const heroFarmland = 'PASTE_URL_HERE';
-export const heroForest = 'PASTE_URL_HERE';
-export const heroMountain = 'PASTE_URL_HERE';
+export const heroMarket = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2011_04_45%20AM.png?v=1748367055294';
+export const heroMonastery = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2001_28_28%20PM.png?v=1748367061222';
+export const heroFarmland = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2001_28_33%20PM.png?v=1748367064887';
+export const heroForest = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2001_47_08%20PM.png?v=1748368280878';
+export const heroMountain = 'https://cdn.glitch.global/926b1852-0bd6-415e-8777-9e387411d153/ChatGPT%20Image%20May%2027%2C%202025%2C%2001_56_40%20PM.png?v=1748368797043E';
 export const heroRiver = 'PASTE_URL_HERE';
 export const heroPort = 'PASTE_URL_HERE';
 export const heroRuin = 'PASTE_URL_HERE';
